c1bbb9cbb305c9ba9242b9c28de0c85a2a0957a9
Ryan Baxter <rbaxter@pivotal.io>
2018-12-18 16:18:06 -0500

    Updating API usage for Boot 2.1.x.

--- spring-cloud-openfeign-core/src/test/java/org/springframework/cloud/openfeign/ribbon/FeignRibbonClientTests.java
+++ spring-cloud-openfeign-core/src/test/java/org/springframework/cloud/openfeign/ribbon/FeignRibbonClientTests.java
@@ -121,7 +121,10 @@ public class FeignRibbonClientTests {
 
 	@Test
 	public void verifyCleanUrl() throws Exception {
-		Request request = new RequestTemplate().method("GET").append("http://tp/abc/bcd.json")
+		Request request = new RequestTemplate()
+				.method(GET)
+				.target("http://tp/abc/bcd.json")
+				.resolve(new HashMap<>())
 				.request();
 		this.client.execute(request, new Options());
 		RequestMatcher matcher = new RequestMatcher("http://foo.com:8000/abc/bcd.json");
