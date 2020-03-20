146e6e48f73a96eefa5fd0ad20f079c953a88309
Michael Cramer <michael.cramer@check24.de>
2020-03-20 12:42:09 +0100

    add support for an FeignErrorDecoderFactory this factory will be used if no ErrorDecoder was found. This factory allows to create a ErrorDecoder based on the type. This fixes #308

---
+++ spring-cloud-openfeign-core/src/test/java/org/springframework/cloud/openfeign/FeignClientErrorDecoderTests.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2020-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.openfeign;
+
+import feign.Contract;
+import feign.RequestLine;
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.openfeign.support.SpringMvcContract;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Michael Cramer
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = FeignClientErrorDecoderTests.TestConfiguration.class)
+@DirtiesContext
+public class FeignClientErrorDecoderTests {
+
+	@Autowired
+	private FeignContext context;
+
+	@Autowired
+	private FooClient foo;
+
+	@Autowired
+	private BarClient bar;
+
+	@Test
+	public void clientsAvailable() {
+		assertThat(this.foo).isNotNull();
+		assertThat(this.bar).isNotNull();
+	}
+
+	@Test
+	public void overrideErrorDecoder() {
+		ErrorDecoder.Default.class
+				.cast(this.context.getInstance("foo", ErrorDecoder.class));
+		ErrorDecoderImpl.class.cast(this.context.getInstance("bar", ErrorDecoder.class));
+	}
+
+	@Configuration(proxyBeanMethods = false)
+	@EnableFeignClients(clients = { FooClient.class, BarClient.class })
+	@EnableAutoConfiguration
+	protected static class TestConfiguration {
+
+	}
+
+	@FeignClient(name = "foo", url = "http://foo", configuration = FooConfiguration.class)
+	interface FooClient {
+
+		@RequestLine("GET /")
+		String get();
+
+	}
+
+	public static class FooConfiguration {
+
+		@Bean
+		Contract feignContract() {
+			return new Contract.Default();
+		}
+
+		@Bean
+		FeignErrorDecoderFactory errorDecoderFactory() {
+			return new FeignErrorDecoderFactoryImpl();
+		}
+
+		@Bean
+		ErrorDecoder feignErrorDecoder() {
+			return new ErrorDecoder.Default();
+		}
+
+	}
+
+	@FeignClient(name = "bar", url = "http://bar", configuration = BarConfiguration.class)
+	interface BarClient {
+
+		@RequestMapping(value = "/", method = RequestMethod.GET)
+		String get();
+
+	}
+
+	public static class BarConfiguration {
+
+		@Bean
+		Contract feignContract() {
+			return new SpringMvcContract();
+		}
+
+		@Bean
+		FeignErrorDecoderFactory errorDecoderFactory() {
+			return new FeignErrorDecoderFactoryImpl();
+		}
+
+	}
+
+	public static class FeignErrorDecoderFactoryImpl implements FeignErrorDecoderFactory {
+
+		@Override
+		public ErrorDecoder create(final Class<?> type) {
+			return new ErrorDecoderImpl();
+		}
+
+	}
+
+	public static class ErrorDecoderImpl implements ErrorDecoder {
+
+		@Override
+		public Exception decode(final String methodKey, final Response response) {
+			return null;
+		}
+
+	}
+
+}
