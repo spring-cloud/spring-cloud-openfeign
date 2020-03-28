f965475a08186dc5d5c867b6a6ee36b10c59db19
Darren Foong <darrenfoong@gmail.com>
2020-03-28 23:39:24 +0800

    Update Pojo

--- spring-cloud-openfeign-core/src/test/java/org/springframework/cloud/openfeign/valid/FeignClientTests.java
+++ spring-cloud-openfeign-core/src/test/java/org/springframework/cloud/openfeign/valid/FeignClientTests.java
@@ -1151,9 +1151,35 @@ public class FeignClientTests {
 		private String value2;
 		private Integer value3;
 
+		public Pojo() {};
+
 		public Pojo(String value1, String value2, Integer value3) {
 			this.value1 = value1;
 			this.value2 = value2;
+			this.value3 = value3;
+		}
+
+		public String getValue1() {
+			return value1;
+		}
+
+		public void setValue1(String value1) {
+			this.value1 = value1;
+		}
+
+		public String getValue2() {
+			return value2;
+		}
+
+		public void setValue2(String value2) {
+			this.value2 = value2;
+		}
+
+		public Integer getValue3() {
+			return value3;
+		}
+
+		public void setValue3(Integer value3) {
 			this.value3 = value3;
 		}
 	}
