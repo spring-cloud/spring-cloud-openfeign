07b626e71ffc22a401321a17052d4956221f9d65
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2021-01-25 13:39:05 -0800

    MetricsConfigurationTest -> MetricsConfigurationTests

--- spring-cloud-openfeign-core/src/test/java/org/springframework/cloud/openfeign/MetricsConfigurationTests.java
+++ spring-cloud-openfeign-core/src/test/java/org/springframework/cloud/openfeign/MetricsConfigurationTests.java
@@ -24,7 +24,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * @author Jonatan Ivanov
  */
-class MetricsConfigurationTest {
+class MetricsConfigurationTests {
 
 	@Test
 	void shouldBeEnabledByDefault() {
