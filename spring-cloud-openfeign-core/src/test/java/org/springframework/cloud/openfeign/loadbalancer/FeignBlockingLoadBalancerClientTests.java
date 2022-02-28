1c9d37e812d1dad63a73bbae64e2a4681f27f21d
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2022-02-28 16:18:57 +0100

    Use raw types in ResponseData.

--- spring-cloud-openfeign-core/src/test/java/org/springframework/cloud/openfeign/loadbalancer/FeignBlockingLoadBalancerClientTests.java
+++ spring-cloud-openfeign-core/src/test/java/org/springframework/cloud/openfeign/loadbalancer/FeignBlockingLoadBalancerClientTests.java
@@ -167,11 +167,13 @@ class FeignBlockingLoadBalancerClientTests {
 				.get("anotherLoadBalancerLifecycle")).getCompleteLog().values();
 		assertThat(lifecycleLogRequests).extracting(lbRequest -> lbRequest.getContext().getHint())
 				.contains(callbackTestHint);
-		assertThat(lifecycleLogStartedRequests).extracting(lbRequest -> lbRequest.getContext().getHint())
-				.contains(callbackTestHint);
+		assertThat(lifecycleLogStartedRequests).extracting(lbRequest -> lbRequest.getContext()
+				.getHint())
+			.contains(callbackTestHint);
 		assertThat(anotherLifecycleLogRequests)
-				.extracting(completionContext -> completionContext.getClientResponse().getHttpStatus())
-				.contains(HttpStatus.OK);
+			.extracting(completionContext -> completionContext.getClientResponse()
+				.getHttpStatus())
+			.contains(HttpStatus.OK.value());
 	}
 
 	private String read(Response response) throws IOException {
