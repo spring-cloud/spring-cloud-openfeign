84661728eb6b7f9d83ae508d9bced3cd1c6336fb
Bruce Stewart <bandi.stewart@gmail.com>
2025-05-08 09:31:04 +0200

    Add @author javadoc Update license comments to 2025

--- spring-cloud-openfeign-core/src/test/java/org/springframework/cloud/openfeign/support/PageJacksonModuleTests.java
+++ spring-cloud-openfeign-core/src/test/java/org/springframework/cloud/openfeign/support/PageJacksonModuleTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2024 the original author or authors.
+ * Copyright 2013-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,6 +42,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @author Olga Maciaszek-Sharma
  * @author Pedro Mendes
  * @author Nikita Konev
+ * @author Bruce Stewart
  */
 class PageJacksonModuleTests {
 
